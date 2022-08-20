171ebec609c502daba2c3d68537e0664b525261d
Battlemech <58076232+Battlemech@users.noreply.github.com>
2022-08-20 14:28:25 +0200

    implemented request broadcast

--- Main/Networking/Messaging/Server/MessageServer.cs
+++ Main/Networking/Messaging/Server/MessageServer.cs
@@ -1,4 +1,7 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using Main.Submodules.NetCoreServer;
 
 namespace Main.Networking.Messaging.Server
@@ -40,7 +43,47 @@ namespace Main.Networking.Messaging.Server
 
             return success;
         }
+
+        public delegate void OnReply<T>(List<T> replies) where T : ReplyMessage;
         
+        public bool SendRequests<TRequest, TReply>(TRequest request, OnReply<TReply> onReply)
+            where TReply : ReplyMessage
+            where TRequest : RequestMessage<TReply>
+        {
+            bool success = true;
+            List<MessageSession> sessions = new List<MessageSession>(Sessions.Values.Cast<MessageSession>());
+            List<TReply> replies = new List<TReply>(sessions.Count);
+
+            foreach (var session in sessions)
+            {
+                success = success && session.SendRequest<TRequest, TReply>(request, (reply) =>
+                {
+                    replies.Add(reply);
+                    
+                    //wait for all replies to arrive
+                    if(replies.Count != sessions.Count) return;
+                    
+                    onReply.Invoke(replies);
+                });
+            }
+
+            return success;
+        }
+
+        protected override void OnConnected(TcpSession session)
+        {
+            if (session is not MessageSession messageSession)
+                throw new Exception(
+                    $"Expected connected session to be of type MessageSession, but is {session?.GetType()}");
+            
+            OnConnected(messageSession);
+        }
+
+        protected virtual void OnConnected(MessageSession session)
+        {
+            
+        }
+
         protected override TcpSession CreateSession()
         {
             return new MessageSession(this);
---
+++ Main/Networking/Synchronisation/Server/OnConnect.cs
@@ -0,0 +1,13 @@
+﻿using Main.Networking.Messaging.Server;
+using Main.Submodules.NetCoreServer;
+
+namespace Main.Networking.Synchronisation.Server
+{
+    public partial class SynchronisedServer
+    {
+        protected override void OnConnected(MessageSession session)
+        {
+            
+        }
+    }
+}
