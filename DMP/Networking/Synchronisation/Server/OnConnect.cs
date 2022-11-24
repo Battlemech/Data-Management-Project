b0c64f95ccacb0480c8abc3dcf2b6fa6357f805f
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2022-11-24 10:53:02 +0100

    testSimpleConnect doesn't fail anymore

--- DMP/Networking/Synchronisation/Server/OnConnect.cs
+++ DMP/Networking/Synchronisation/Server/OnConnect.cs
@@ -20,8 +20,6 @@ namespace DMP.Networking.Synchronisation.Server
                 modCount = new Dictionary<string, Dictionary<string, uint>>(_modCount);
             }
 
-            Console.WriteLine($"Server: Starting to send requests at {DateTime.Now}");
-            
             //send a request for each database
             foreach (var kv in modCount)
             {
@@ -34,8 +32,6 @@ namespace DMP.Networking.Synchronisation.Server
                 //replies contain all values which are currently not being modified on the client
                 SendRequestsToOthers<GetValueRequest, GetValueReply>(request, session, replies =>
                 {
-                    Console.WriteLine($"Server: Received all clients replies at {DateTime.Now}");
-                    
                     //get all received replies
                     List<SetValueMessage> setValueMessages = new List<SetValueMessage>();
                     foreach (var reply in replies)
@@ -55,14 +51,12 @@ namespace DMP.Networking.Synchronisation.Server
                     //filter duplicate SetValueMessages and messages with a lower modCount
                     foreach (var message in FilterMessages(setValueMessages))
                     {
-                        Console.WriteLine($"Informing client of {message.GetContent<string>()}");
                         //forward them to the newly connected client
                         session.SendMessage(message);
                     }
                 });
             }
             
-            Console.WriteLine($"Server: Sent requests at {DateTime.Now}");
         }
 
         private List<SetValueMessage> FilterMessages(List<SetValueMessage> messages)
